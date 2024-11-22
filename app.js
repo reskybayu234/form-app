c10e0d8af0872e486e0cb637f6119fa2efeeb203
bayutrillium <75589544+reskybayu234@users.noreply.github.com>
2024-11-22 07:14:31 +0700

    router

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 import express from "express";
+import apiRouter from "./routes/api.js";
 
 const app = express();
+
+/** 
+ * parse application/json
+ * parse application/x-www-form-urlencoded
+ * parse multipart/form-data
+ */
+app.use(express.json()); // ini untuk parse application/json agar bisa di akses di body dalam bentuk json
+app.use(express.urlencoded({ extended: true })); // ini untuk parse application/x-www-form-urlencoded agar bisa di akses di body dalam bentuk object
+
+app.use("/api", apiRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
